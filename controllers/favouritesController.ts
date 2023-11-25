c6d6cd1a340c1a445060d213d7d7d4c4cf20048c
ad.hanif <ad.hanif@outlook.com>
2023-11-25 23:20:04 +0100

    add, get and delete favourites implemented

--- controllers/favouritesController.ts
+++ controllers/favouritesController.ts
@@ -6,14 +6,20 @@ export const addFavourites = async (
   res: express.Response
 ) => {
   try {
-    console.log(req.body);
     const { cityname } = req.body;
     const { id } = (req as any).user;
-    const newFavourite = await Favourites.create({
-      cityname,
-      creater: id,
-    });
-    res.status(201).json(newFavourite);
+
+    // console.log(cityname, id);
+    const alreadyFavourite = await Favourites.findOne({ cityname });
+
+    if (!alreadyFavourite) {
+      const newFavourite = await Favourites.create({ cityname, creater: id });
+      res.status(200).json(newFavourite);
+    } else {
+      res
+        .status(400)
+        .json({ error: "You have already added to your favourites" });
+    }
   } catch (error: unknown) {
     let errorMessage = "Something went wrong";
     if (error instanceof Error) {
@@ -31,6 +37,31 @@ export const getFavouritesOfUser = async (
   try {
     const getFavourites = await Favourites.find({ creater: id });
     res.status(201).json(getFavourites);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong";
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    res.status(400).send({ error: errorMessage });
+  }
+};
+
+export const deleteOneFavourite = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const { id } = req.params;
+  const result = await Favourites.deleteOne({ _id: id });
+
+  if (result.deletedCount === 1) {
+    res.status(200).json({ message: "Favourite deleted successfully" });
+  } else {
+    res.status(404).json({ error: "Favourite not found" });
+  }
+
+  try {
+    // const getFavourites = await Favourites.find({ creater: id });
+    // res.status(201).json(getFavourites);
   } catch (error: unknown) {
     let errorMessage = "Something went wrong";
     if (error instanceof Error) {
--- routes/favouritesRoute.ts
+++ routes/favouritesRoute.ts
@@ -1,10 +1,16 @@
 import express from "express";
 const favouritesRouter = express.Router();
-import { getFavouritesOfUser, addFavourites } from "../controllers/favouritesController";
+import {
+  getFavouritesOfUser,
+  addFavourites,
+  deleteOneFavourite,
+} from "../controllers/favouritesController";
 
 import { verfiyToken } from "../middlewares/verifyToken";
 
 favouritesRouter.post("/", verfiyToken, addFavourites);
 favouritesRouter.get("/", verfiyToken, getFavouritesOfUser);
+
+favouritesRouter.delete("/:id", verfiyToken, deleteOneFavourite);
 
 export default favouritesRouter;
